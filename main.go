01a143c1d95e314106caf8eed3d27bda65565e92
kentaiwami <kentaiwami12@gmail.com>
2026-04-30 22:19:55 +0900

    commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+type Reading struct {
+	Value *float64 `json:"value"`
+}
+
+var db *sql.DB
+
+func postReading(table string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		var data Reading
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, "invalid request body", http.StatusBadRequest)
+			return
+		}
+		if data.Value == nil {
+			http.Error(w, "value is required", http.StatusBadRequest)
+			return
+		}
+		_, err := db.Exec("INSERT INTO "+table+" (value) VALUES (?)", data.Value)
+		if err != nil {
+			log.Printf("db insert error: %v", err)
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func main() {
+	godotenv.Load()
+
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = "sensor:sensor@tcp(localhost:3306)/sensordb?parseTime=true"
+	}
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	http.HandleFunc("/temperature", postReading("temperatures"))
+	http.HandleFunc("/humidity", postReading("humidities"))
+	http.HandleFunc("/co2", postReading("co2s"))
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
